7fa38f4d208942155e8037f75fdb94847a358b7d
LYMDLUT <70597027+LYMDLUT@users.noreply.github.com>
2024-01-18 18:36:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,116 @@
-# 导入所需的库
-from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
-import torch
+from dataclasses import asdict
 import streamlit as st
+import torch
+from interface import GenerationConfig, generate_interactive
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers.utils import logging
 
 from modelscope import snapshot_download
+model_id = 'Shanghai_AI_Laboratory/internlm2-chat-20b'
+mode_name_or_path = snapshot_download(model_id, revision='master')
+logger = logging.get_logger(__name__)
 
 def on_btn_click():
     del st.session_state.messages
-# 在侧边栏中创建一个标题和一个链接
-with st.sidebar:
-    max_length = st.slider("Max Length", min_value=32, max_value=2048, value=2048)
-    top_p = st.slider("Top P", 0.0, 1.0, 0.8, step=0.01)
-    temperature = st.slider("Temperature", 0.0, 1.0, 0.7, step=0.01)
-    st.button("Clear Chat History", on_click=on_btn_click)
-    system_prompt = st.text_input("System_Prompt", "你是一个人工智能助手")
-
-# 创建一个标题和一个副标题
-st.title("💬 InternLM2-Chat-20B-4bits")
-st.caption("🚀 A streamlit chatbot powered by InternLM2")
-
-# 定义模型路径
-
-model_id = 'Shanghai_AI_Laboratory/internlm2-chat-20b'
-
-mode_name_or_path = snapshot_download(model_id, revision='master')
 
 
-# 定义一个函数，用于获取模型和tokenizer
 @st.cache_resource
-def get_model():
-    # 从预训练的模型中获取tokenizer
+def load_model():
+    model = (
+        AutoModelForCausalLM.from_pretrained(mode_name_or_path, load_in_4bit=True, trust_remote_code=True)
+        .to(torch.bfloat16)
+        .cuda()
+    )
     tokenizer = AutoTokenizer.from_pretrained(mode_name_or_path, trust_remote_code=True)
-    # 从预训练的模型中获取模型，并设置模型参数
-    model = AutoModelForCausalLM.from_pretrained(mode_name_or_path, trust_remote_code=True, load_in_4bit=True, device_map="auto")
-    model.eval()  
-    return tokenizer, model
+    return model, tokenizer
 
-# 加载InternLM的model和tokenizer
-tokenizer, model = get_model()
 
-# 如果session_state中没有"messages"，则创建一个包含默认消息的列表
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+def prepare_generation_config():
+    with st.sidebar:
+        max_length = st.slider("Max Length", min_value=32, max_value=2048, value=2048)
+        top_p = st.slider("Top P", 0.0, 1.0, 0.8, step=0.01)
+        temperature = st.slider("Temperature", 0.0, 1.0, 0.7, step=0.01)
+        st.button("Clear Chat History", on_click=on_btn_click)
 
-# 遍历session_state中的所有消息，并显示在聊天界面上
-for msg in st.session_state.messages:
-    st.chat_message("user").write(msg[0])
-    st.chat_message("assistant").write(msg[1])
+    generation_config = GenerationConfig(max_length=max_length, top_p=top_p, temperature=temperature)
 
-# 如果用户在聊天输入框中输入了内容，则执行以下操作
-if prompt := st.chat_input():
-    # 在聊天界面上显示用户的输入
-    st.chat_message("user").write(prompt)
-    # 构建输入     
-    response, history = model.chat(tokenizer, prompt, meta_instruction=system_prompt, history=st.session_state.messages)
-    # 将模型的输出添加到session_state中的messages列表中
-    st.session_state.messages.append((prompt, response))
-    # 在聊天界面上显示模型的输出
-    st.chat_message("assistant").write(response)
+    return generation_config
+
+
+user_prompt = "[UNUSED_TOKEN_146]user\n{user}[UNUSED_TOKEN_145]\n"
+robot_prompt = "[UNUSED_TOKEN_146]assistant\n{robot}[UNUSED_TOKEN_145]\n"
+cur_query_prompt = "[UNUSED_TOKEN_146]user\n{user}[UNUSED_TOKEN_145]\n[UNUSED_TOKEN_146]assistant\n"
+
+
+def combine_history(prompt):
+    messages = st.session_state.messages
+    total_prompt = ""
+    for message in messages:
+        cur_content = message["content"]
+        if message["role"] == "user":
+            cur_prompt = user_prompt.replace("{user}", cur_content)
+        elif message["role"] == "robot":
+            cur_prompt = robot_prompt.replace("{robot}", cur_content)
+        else:
+            raise RuntimeError
+        total_prompt += cur_prompt
+    total_prompt = total_prompt + cur_query_prompt.replace("{user}", prompt)
+    return total_prompt
+
+
+def main():
+    # torch.cuda.empty_cache()
+    print("load model begin.")
+    model, tokenizer = load_model()
+    print("load model end.")
+
+    user_avator = "assets/user.png"
+    robot_avator = "assets/robot.png"
+
+    st.title("InternLM2-Chat-7B")
+
+    generation_config = prepare_generation_config()
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"], avatar=message.get("avatar")):
+            st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("What is up?"):
+        # Display user message in chat message container
+        with st.chat_message("user", avatar=user_avator):
+            st.markdown(prompt)
+        real_prompt = combine_history(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt, "avatar": user_avator})
+
+        with st.chat_message("robot", avatar=robot_avator):
+            message_placeholder = st.empty()
+            for cur_response in generate_interactive(
+                model=model,
+                tokenizer=tokenizer,
+                prompt=real_prompt,
+                additional_eos_token_id=92542,
+                **asdict(generation_config),
+            ):
+                # Display robot response in chat message container
+                message_placeholder.markdown(cur_response + "▌")
+            message_placeholder.markdown(cur_response)  # pylint: disable=undefined-loop-variable
+        # Add robot response to chat history
+        st.session_state.messages.append(
+            {
+                "role": "robot",
+                "content": cur_response,  # pylint: disable=undefined-loop-variable
+                "avatar": robot_avator,
+            }
+        )
+        torch.cuda.empty_cache()
+
+
+if __name__ == "__main__":
+    main()
